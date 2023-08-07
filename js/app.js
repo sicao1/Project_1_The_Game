5914ea4b6f5ca85238ae2418d67016572ac4a521
sicao1 <singacao85@gmail.com>
2023-08-07 14:05:13 -0500

    able to loop through all selection of characters

--- js/app.js
+++ js/app.js
@@ -1,19 +1,38 @@
-const image = document.querySelector("#McDonalds");
-const showImage = document.querySelector("#select-McDonalds");
+const imagePairTags = [
+  ["#McDonalds", "#select-McDonalds"],
+  ["#InNOut", "#select-InNOut"],
+  ["#PandaExpress", "#select-PandaExpress"],
+  ["#Popeyes", "#select-popeyes"],
+  ["#Wendys", "#select-wendys"],
+  ["#BurgerKing", "#select-burgerKing"],
+  ["#KFC", "#select-kfc"],
+  ["#Mark", "#select-wahlburgers"],
+  ["#Chipotle", "#select-chipotle"],
+  ["#TacoBell", "#select-tacoBell"],
+];
 
-image.addEventListener("mouseover", function () {
-  showImage.classList.toggle("hidden");
-});
-image.addEventListener("mouseout", function () {
-  showImage.classList.toggle("hidden");
-});
+for (const [imageSelection, targetSelection] of imagePairTags) {
+  const image = document.querySelector(imageSelection);
+  const showImage = document.querySelector(targetSelection);
 
-const image2 = document.querySelector("#InNOut");
-const showImage2 = document.querySelector("#select-InNOut");
+  image.addEventListener("mouseover", function () {
+    showImage.classList.toggle("hidden");
+  });
+  image.addEventListener("mouseout", function () {
+    showImage.classList.toggle("hidden");
+  });
+}
 
-image2.addEventListener("mouseover", function () {
-  showImage2.classList.toggle("hidden");
-});
-image2.addEventListener("mouseout", function () {
-  showImage2.classList.toggle("hidden");
-});
+// const image = document.querySelector("#McDonalds");
+
+// const showImage = document.querySelector("#select-McDonalds");
+
+// const image2 = document.querySelector("#InNOut");
+// const showImage2 = document.querySelector("#select-InNOut");
+
+// image2.addEventListener("mouseover", function () {
+//   showImage2.classList.toggle("hidden");
+// });
+// image2.addEventListener("mouseout", function () {
+//   showImage2.classList.toggle("hidden");
+// });
