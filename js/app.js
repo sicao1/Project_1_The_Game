591cbd90185a022e3dca700a52f44bd4ccf4e0f9
sicao1 <singacao85@gmail.com>
2023-08-07 20:45:23 -0500

    player 2 can choose, need to figure out how to do one at a time

--- js/app.js
+++ js/app.js
@@ -11,7 +11,6 @@ const imagePairTags = [
   ["#Chipotle", "#select-chipotle"],
   ["#TacoBell", "#select-tacoBell"],
 ];
-
 const chooseFighter = () => {
   let clicked = false;
 
@@ -32,6 +31,47 @@ const chooseFighter = () => {
     image.addEventListener("click", function () {
       if (!clicked) {
         showImage.classList.remove("hidden");
+        console.log(showImage);
+        clicked = true;
+      }
+    });
+  }
+};
+
+const imagePairTags2 = [
+  ["#McDonalds", "#select-McDonalds2"],
+  ["#InNOut", "#select-InNOut2"],
+  ["#PandaExpress", "#select-PandaExpress2"],
+  ["#Popeyes", "#select-popeyes2"],
+  ["#Wendys", "#select-wendys2"],
+  ["#BurgerKing", "#select-burgerKing2"],
+  ["#KFC", "#select-kfc2"],
+  ["#Mark", "#select-wahlburgers2"],
+  ["#Chipotle", "#select-chipotle2"],
+  ["#TacoBell", "#select-tacoBell2"],
+];
+
+const chooseFighter2 = () => {
+  let clicked = false;
+
+  for (const [imageSelection, targetSelection] of imagePairTags2) {
+    const image = document.querySelector(imageSelection);
+    const showImage = document.querySelector(targetSelection);
+
+    image.addEventListener("mouseover", function () {
+      if (!clicked) {
+        showImage.classList.toggle("hidden");
+      }
+    });
+    image.addEventListener("mouseout", function () {
+      if (!clicked) {
+        showImage.classList.toggle("hidden");
+      }
+    });
+    image.addEventListener("click", function () {
+      if (!clicked) {
+        showImage.classList.remove("hidden");
+        console.log(showImage);
         clicked = true;
       }
     });
@@ -40,3 +80,17 @@ const chooseFighter = () => {
 
 // Starting Conditions
 //  Player one to choose fighter
+const player1Choose = () => {
+  document.querySelector(".player-1");
+  chooseFighter();
+};
+
+const player2Choose = () => {
+  document.querySelector(".player-2");
+  chooseFighter2();
+};
+
+chooseFighter2();
+
+// const source = document.querySelector(".selector-2 img");
+// source.src = "imgs/TacoBell.png";
